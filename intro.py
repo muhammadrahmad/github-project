47d8d6640c46431d88b5312047e182970a4a950d
muhammadrahmad <muhammadrahmad12tkj.com>
2025-03-24 16:02:50 +0700

    Initial commit

---
+++ intro.py
@@ -0,0 +1,14 @@
+
+nama = input("Masukkan nama Anda: ")
+umur = input("Masukkan umur Anda: ")
+universitas = input("Masukkan universitas atau sekolah Anda: ")
+jurusan = input("Masukkan jurusan Anda: ")
+hobi = input("Masukkan hobi Anda: ")
+
+
+print("\nHalo, nama saya", nama + ".")
+print("Saya berusia", umur, "tahun dan merupakan mahasiswa dari", universitas + ", jurusan", jurusan + ".")
+print("Saya memiliki minat yang besar dalam belajar dan mengeksplorasi ilmu baru.")
+print("Di waktu luang, saya menikmati", hobi + ".")
+print("Saya selalu bersemangat untuk meningkatkan diri dan mendapatkan pengalaman baru.")
+print("Senang bertemu dengan Anda!")
